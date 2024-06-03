8cf408c3c0fd58a14c42431a8bce581c68347e58
dalleth-martinss <essi_dalleth@outlook.com>
2024-06-02 21:24:02 -0300

    First exercise with Enums

---
+++ Entities/Order.cs
@@ -0,0 +1,24 @@
+﻿using ExercicioEnum.Entities.Enums;
+using System;
+using System.Collections.Generic;
+
+
+namespace ExercicioEnum.Entities
+{
+    internal class Order
+    {
+        public int Id { get; set; }
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+
+        public override string ToString()
+        {
+            return Id
+                + ", "
+                + Moment
+                + ", "
+                + Status;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using ExercicioEnum.Entities;
+using ExercicioEnum.Entities.Enums;
+using System;
+
+
+namespace ExercicioEnum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayment.ToString(); // conversão de enumerado p/ string
+            Console.WriteLine(txt);
+
+            //OrderStatus os = Enum.Parse(OrderStatus ("Delivered"));
+            // Console.WriteLine(os); // coloquei essa linha aqui para que o CMD não fechasse rapidamente
+
+
+            OrderStatus os = (OrderStatus)Enum.Parse(typeof(OrderStatus), "Delivered");
+            Console.WriteLine(os);
+
+            Console.ReadLine();
+        }
+    }
+}
